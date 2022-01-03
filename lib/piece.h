2f83e5ac2c5cb2d610568a35c481cf3852ba6f9b
tkx99 <55948297+tkx99@users.noreply.github.com>
2022-01-03 12:56:24 +0100

    second implementation of piece.h

--- lib/piece.h
+++ lib/piece.h
@@ -2,23 +2,29 @@
 #define PIECE_H
 
 #include <string>
+#include <stdexcept>
+#include <math>
 
 class piece{
 	public:
-		Piece(char col, short int r, short int c);
+		piece(char name, short int r, short int c);
+
+		piece(const piece& p); //costruttore copia
+		piece(piece&& p); //costruttore spostamento
+		
+		piece& operator=(const piece& p); //assegnamento copia
+		piece& operator=(piece&& p); //assegnamento di spostamkento
+		
+		short int[] get_position(); //il primo numero è l'indice colonna, il secondo quello riga
+		void set_position(short int c, short int r); 
+		
+		virtual bool is_valid_final_pos(short int c, short int r) = 0;
+		
+		string to_string() = 0;
+		
 		virtual ~piece() = 0;
-		
-		bool is_same_color(const piece& p);
-		virtual bool is_valid_move(char r, char c) = 0;
-		
-		char get_row();
-		char get_column();
-		void set_row(short int r);
-		void set_column(short int c);
-		
-		virtual string to_string() = 0;
 	private:
-		char color;
+		char piece_name; //il char permette anche la distinzione dei pezzi bianchi (lettere maiuscole) da quelli neri (lettere minuscole)
 		short int row;
 		short int column;
 }
