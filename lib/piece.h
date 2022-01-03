7f486095e29de3f77dc46ee5044f271555c036d6
tkx99 <55948297+tkx99@users.noreply.github.com>
2022-01-03 12:59:24 +0100

    corrections

--- lib/piece.h
+++ lib/piece.h
@@ -11,17 +11,17 @@ class piece{
 
 		piece(const piece& p); //costruttore copia
 		piece(piece&& p); //costruttore spostamento
-		
+
 		piece& operator=(const piece& p); //assegnamento copia
 		piece& operator=(piece&& p); //assegnamento di spostamkento
-		
+
 		short int[] get_position(); //il primo numero è l'indice colonna, il secondo quello riga
-		void set_position(short int c, short int r); 
-		
+		void set_position(short int c, short int r);
+
 		virtual bool is_valid_final_pos(short int c, short int r) = 0;
-		
-		string to_string() = 0;
-		
+
+		string to_string();
+
 		virtual ~piece() = 0;
 	private:
 		char piece_name; //il char permette anche la distinzione dei pezzi bianchi (lettere maiuscole) da quelli neri (lettere minuscole)
