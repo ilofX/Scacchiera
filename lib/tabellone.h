65c05c842027343df21d2fe6dce251d9c2feb09e
Filippo Stella <filippo.stella.360@gmail.com>
2022-01-04 20:31:41 +0100

    Implemented functionObject for king and pieces research. Created getKing and getPiece functions. Implemented deleteHistory function. Implemented print function. Started Implementing hasNextMove function.
    Edited CMakeLists.txt to optimize library building
    
    Signed-off-by: Filippo Stella <filippo.stella.360@gmail.com>

--- lib/tabellone.h
+++ lib/tabellone.h
@@ -9,6 +9,7 @@
 #include <list>
 #include <vector>
 #include <algorithm>
+#include "piece.h"
 
 class tabellone{
 public:
@@ -26,24 +27,50 @@ public:
     tabellone& operator=(tabellone&& tab);
 
     class IllegalMoveException: public std::exception{};
+    class IllegalCoordinatesException: public std::exception{};
     class MatchTiedException: public std::exception{};
+    class InvalidStateException: public std::exception{};
     class CheckException: public std::exception{};
     class CheckmateException: public std::exception{};
+    class PromotionException: public std::exception{};
 
-    std::string hasNextMove() const;
+    class findPiece{
+    public:
+        findPiece(char column, char row):column{column},row{row}{};
+        bool operator() (piece Piece) const{
+            return (Piece.get_row()==row && Piece.get_column()==column);
+        }
+    private:
+        char column=0, row=0;
+    };
+
+    class findKing{
+    public:
+        findKing(char pieceName) :pieceName{pieceName}{};
+        bool operator() (piece Piece) const{
+            return Piece.get_piece_name()==pieceName;
+        }
+    private:
+        char pieceName=0;
+    };
+
+    bool hasNextMove() const;
     std::string move(char startColumn, char startRow, char endColumn, char endRow);
     std::string promotion(char Column, char Row, char piece);
 
 private:
     std::string print();
+    const piece & getKing(bool isBlack) const;
+    const piece& getPiece(char column, char row);
+    bool canMove(piece& piece) const;
     bool isCheck(char column, char row) const;
-    bool isCheckmate(char column, char row) const;
+    bool isCheckmate(const piece& king) const;
     bool isTie() const;
-    bool removePiece(char cloumn, char row);
-    std::vector<std::string> deleteHistory(std::vector<std::string>::iterator from, std::vector<std::string>::iterator to);
+    bool removePiece(char column, char row);
+    void deleteHistory();
 
     //Giocatore& whitePlayer, blackPlayer;
-    //std::list<Pezzo> whitePieces, blackPieces;
+    std::list<piece> whitePieces, blackPieces;
     std::vector<std::string> history;
     int turn=-1;
 };
--- src/tabellone.cpp
+++ src/tabellone.cpp
@@ -2,7 +2,9 @@
 // Created by Filippo Stella 2052644 on 22/12/2021.
 //
 
+#include <algorithm>
 #include "tabellone.h"
+#include "piece.h"
 
 
 std::string tabellone::move(char startColumn, char startRow, char endColumn, char endRow) {
@@ -10,25 +12,78 @@ std::string tabellone::move(char startColumn, char startRow, char endColumn, cha
 }
 
 std::string tabellone::print() {
-    return std::__cxx11::string();
+    std::string ris;
+    char matr[8][8] {' '};
+    for(auto blackPiece=blackPieces.begin();blackPiece!=blackPieces.end();blackPiece++){
+        matr[blackPiece->get_column()][blackPiece->get_row()]=blackPiece->get_piece_name();
+    }
+    for(auto whitePiece=whitePieces.begin();whitePiece!=whitePieces.end();whitePiece++){
+        matr[whitePiece->get_column()][whitePiece->get_row()]=whitePiece->get_piece_name();
+    }
+    for(int i=8; i>0; i--){
+        ris+=to_string(i)+" ";
+        for(int j=0;j<8;j++){
+            ris+=matr[j][i];
+        }
+        ris+='\n';
+    }
+    ris+="ABCDEFGH";
+    return ris;
 }
 
 std::string tabellone::promotion(char Column, char Row, char piece) {
     return std::__cxx11::string();
 }
 
-std::string tabellone::hasNextMove() const {
-    return std::__cxx11::string();
+bool tabellone::hasNextMove() const {
+    if(isTie()) throw MatchTiedException();
+    if(isCheckmate(getKing(false))) throw CheckmateException();
+    if(isCheckmate(getKing(true))) throw CheckmateException();
+    return true;
 }
 
 bool tabellone::isTie() const {
     return false;
 }
 
-bool tabellone::isCheckmate(char column, char row) const {
+bool tabellone::isCheckmate(const piece &king) const {
     return false;
 }
 
 bool tabellone::isCheck(char column, char row) const {
     return false;
+}
+
+bool tabellone::removePiece(char column, char row) {
+    return false;
+}
+
+void tabellone::deleteHistory() {
+    history.clear();
+}
+
+bool tabellone::canMove(piece& piece) const {
+    return false;
+}
+
+const piece& tabellone::getKing(bool isBlack) const {
+    if(isBlack){
+        auto iter = std::find_if(blackPieces.begin(),blackPieces.end(),findKing('K'));
+        if(iter!=blackPieces.end()) return *iter;
+        throw InvalidStateException();
+    }
+    else{
+        auto iter = std::find_if(whitePieces.begin(),whitePieces.end(),findKing('k'));
+        if(iter!=whitePieces.end()) return *iter;
+        throw InvalidStateException();
+    }
+}
+
+const piece &tabellone::getPiece(char column, char row) {
+    auto iter = std::find_if(whitePieces.begin(),whitePieces.end(),findPiece(column,row));
+    if(iter!=whitePieces.end()) return *iter;
+    iter = std::find_if(blackPieces.begin(),blackPieces.end(),findPiece(column,row));
+    if(iter!=blackPieces.end()) return *iter;
+
+    throw IllegalCoordinatesException();
 }
