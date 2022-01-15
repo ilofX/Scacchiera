6c670407a193e9e3251f091e436d3d6e08ff2e2c
Filippo Stella <filippo.stella.360@gmail.com>
2022-01-15 13:33:06 +0100

    Started writing main structure and parameter checks and conditions
    Signed-off-by: Filippo Stella <filippo.stella.360@gmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,28 @@
 #include <iostream>
+#include <string>
 #include "tabellone.h"
 #include "player.h"
 
-int main() {
-    tabellone board = tabellone();
 
+int main(int argc, char* argv[]) {
+    //Argument checking
+    if(argc<1 || argc>2){
+        exit(1);
+    }
+    std::cout << argv[0];
+    //players creation
+    if(argv[0][0]=='c' && argv[0][1]=='c'){ //Computer vs Computer
+
+    }
+    else if(argv[0][0]=='p' && argv[0][1]=='c'){ //Player vs Computer
+
+    }
+    else{
+        exit(2);
+    }
+    //Board Construction
+    tabellone board = tabellone();
+    //Match Loop
     try {
         while (board.hasNextMove()) {
 
@@ -12,34 +30,42 @@ int main() {
 
             }
             catch (tabellone::IllegalMoveException &ex) {
-
+                std::cout << "La mossa richiesta non è valida" << std::endl;
+                continue;
             }
             catch (tabellone::IllegalCoordinatesException &ex) {
-
+                std::cout << "Le coordinate inserite non sono valide" << std::endl;
+                continue;
             }
             catch (tabellone::CheckException &ex) {
                 try { //Attempt possible solution
-                    //move(startColumn, startRow, endColumn, endRow);
+                    //get move
+
+                    //if(board.solvesCheck(startColumn, startRow, endColumn, endRow)) move(startColumn, startRow, endColumn, endRow);
                 }
                 catch(tabellone::IllegalMoveException& ex){
-                    return false;
+                    throw tabellone::CheckException();
                 }
             }
             catch (tabellone::PromotionException &ex) {
+                //Get promotion piece selection
 
+                //board.promotion(endColumn, endRow, promotion);
             }
-
             catch (tabellone::InvalidStateException &ex) {
-
+                std::cout << "La partita ha raggiunto uno stato inconsistente, terminato" << std::endl;
+                exit(-1);
             }
 
         }
     }
     catch (tabellone::CheckmateException &ex) {
-
+        std::cout << "La partita è terminata per scacco matto" << std::endl;
+        exit(40);
     }
     catch (tabellone::MatchTiedException &ex) {
-
+        std::cout << "La partita è terminata per parità" << std::endl;
+        exit(50);
     }
     return 0;
 }
