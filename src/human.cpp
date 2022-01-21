e2da580936ed3bf6c49ac25fd29d65cd1343da95
tkx99 <55948297+tkx99@users.noreply.github.com>
2022-01-21 18:21:47 +0100

    player

--- src/human.cpp
+++ src/human.cpp
@@ -7,19 +7,17 @@
 human::human(char n, board& t) : player(n, t) {}
 
 bool human::move(string s){
-    bool flag = false;
     if(!is_valid_input(s)){
-        throw InvalidMoveException();
+        throw IllegalInputException();
     }else if(is_display_input(s)){
             this->scacchiera.print();
-            throw InvalidMoveException();
+            return false;
             //break;
     }else{
         vector<short int> v = convert_input(s);
         this->scacchiera.move(v[0], v[1], v[2], v[3]);
+        return true;
     }
-
-    return flag;
 }
 
 vector<short int> player::convert_input(string s){ //il vettore tornato contiene [stat_col, start_row, end_col, end_row]
