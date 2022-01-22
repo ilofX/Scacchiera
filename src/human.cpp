c269f50d669e2df25491a7af568eef18200adb9b
tkx99 <55948297+tkx99@users.noreply.github.com>
2022-01-22 18:20:12 +0100

    update covert_input

--- src/human.cpp
+++ src/human.cpp
@@ -23,10 +23,10 @@ vector<short int> human::convert_input(string s){
 	vector<short int> ret;
 
 	if(is_valid_input(s) && !is_display_input(s)){
-		ret.push_back((short int)tolower((int)s[0]) - 96);
-		ret.push_back((short int)s[1] - 48);
-		ret.push_back((short int)tolower((int)s[2]) - 96);
-		ret.push_back((short int)s[3] - 48);
+		ret.push_back((short int)tolower((int)s[0]) - 97);
+		ret.push_back((short int)s[1] - 56);
+		ret.push_back((short int)tolower((int)s[2]) - 97);
+		ret.push_back((short int)s[3] - 56);
 	}
 	return ret;
 }
