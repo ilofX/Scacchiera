b3b37e3651f4699aaa557f1cd5c55104341d416a
Franzesca <96779849+Franzesca@users.noreply.github.com>
2022-01-23 00:57:00 +0100

    Add files via upload

---
+++ lib/writer.h
@@ -0,0 +1,32 @@
+//
+// Created by Francesca Damian
+//
+
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <regex>
+#include <chrono>
+#include <ctime>
+
+class writer {
+public:
+    writer(std::string file);
+
+    bool write_move(std::string move);
+    bool write_exception(std::string exception);
+    bool write_promotion(std::string promotion);
+    
+    class IllegalWriterMoveException : public std::exception {};
+    class IllegalWriterPromotionException : public std::exception {};
+private:
+    std::ofstream output_file;
+    std::regex visualizer_regx{ "[xX][xX][' '][xX][xX]" };
+    std::regex move_regx{ "[a-hA-H][1-8][' '][a-hA-H][1-8]" };
+    std::regex promotion_regx{ "[tTcCaAdD]" };
+
+    std::string move_formatter(std::string unformatted_str);
+    void write_log_start();
+};
